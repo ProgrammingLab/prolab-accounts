aaab759f49a8a5abe41cd03275b41182137b4e82
Ryota Egusa <gedorinku@yahoo.co.jp>
2019-01-18 23:29:31 +0900

    Connect hydra admin api

---
+++ app/di/client_component.go
@@ -0,0 +1,46 @@
+package di
+
+import (
+	"context"
+
+	"github.com/ory/hydra/sdk/go/hydra"
+	"github.com/pkg/errors"
+
+	"github.com/ProgrammingLab/prolab-accounts/app/config"
+)
+
+// ClientComponent is an interface of api clients
+type ClientComponent interface {
+	HydraClient(ctx context.Context) *hydra.CodeGenSDK
+}
+
+// NewClientComponent returns new client component
+func NewClientComponent(cfg *config.Config) (ClientComponent, error) {
+	h, err := newHydraClient(cfg)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return &clientComponentImpl{
+		hydraCli: h,
+	}, nil
+}
+
+func newHydraClient(cfg *config.Config) (*hydra.CodeGenSDK, error) {
+	hc := &hydra.Configuration{
+		AdminURL: cfg.HydraAdminURL,
+	}
+	cli, err := hydra.NewSDK(hc)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	return cli, nil
+}
+
+type clientComponentImpl struct {
+	hydraCli *hydra.CodeGenSDK
+}
+
+func (c *clientComponentImpl) HydraClient(ctx context.Context) *hydra.CodeGenSDK {
+	return c.hydraCli
+}
