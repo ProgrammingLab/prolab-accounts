a9035d44b56dc2d0c57c3ad0b87f3da91e9e8a19
Ryota Egusa <gedorinku@yahoo.co.jp>
2019-01-20 15:48:34 +0900

    Impl StartOauthLogin

--- app/server/oauth_server.go
+++ app/server/oauth_server.go
@@ -4,7 +4,9 @@ import (
 	"context"
 
 	"github.com/izumin5210/grapi/pkg/grapiserver"
+	"github.com/ory/hydra/sdk/go/hydra/swagger"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/grpclog"
 	"google.golang.org/grpc/status"
 
 	api_pb "github.com/ProgrammingLab/prolab-accounts/api"
@@ -29,8 +31,31 @@ type oAuthServiceServerImpl struct {
 }
 
 func (s *oAuthServiceServerImpl) StartOauthLogin(ctx context.Context, req *api_pb.StartOauthLoginRequest) (*api_pb.StartOAuthLoginResponse, error) {
-	// TODO: Not yet implemented.
-	return nil, status.Error(codes.Unimplemented, "TODO: You should implement it!")
+	cli := s.HydraClient(ctx)
+	challenge := req.GetLoginChallenge()
+	res, _, err := cli.GetLoginRequest(challenge)
+	if err != nil {
+		grpclog.Error(err)
+		return nil, err
+	}
+	if res.Skip {
+		res, _, err := cli.AcceptLoginRequest(challenge, swagger.AcceptLoginRequest{})
+		if err != nil {
+			grpclog.Error(err)
+			return nil, err
+		}
+
+		resp := &api_pb.StartOAuthLoginResponse{
+			Skip:        true,
+			RedirectUrl: res.RedirectTo,
+		}
+		return resp, nil
+	}
+
+	resp := &api_pb.StartOAuthLoginResponse{
+		Skip: false,
+	}
+	return resp, nil
 }
 
 func (s *oAuthServiceServerImpl) OAuthLogin(ctx context.Context, req *api_pb.OAuthLoginRequest) (*api_pb.OAuthLoginResponse, error) {
