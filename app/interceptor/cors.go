9a1d2a70a3efcbea4425703c8e41878cc84e98f3
Ryota Egusa <gedorinku@yahoo.co.jp>
2019-01-27 17:58:29 +0900

    CORS

--- app/interceptor/cors.go
+++ app/interceptor/cors.go
@@ -1,6 +1,8 @@
 package interceptor
 
-import "net/http"
+import (
+	"net/http"
+)
 
 // CORSMiddleware repesents CORS middleware
 func CORSMiddleware(hander http.Handler) http.Handler {
@@ -16,7 +18,11 @@ type corsHander struct {
 func (h *corsHander) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	header := w.Header()
 	header.Add("Access-Control-Allow-Origin", "*")
-	header.Add("Access-Control-Allow-Methods", "GET,HEAD,POST,PUT,DELETE,PATCH")
-	header.Add("Access-Control-Allow-Headers", "Authorization")
+	if req.Method == "OPTIONS" && req.Header.Get("Access-Control-Request-Method") != "" {
+		header.Add("Access-Control-Allow-Methods", "GET,HEAD,POST,PUT,DELETE,PATCH")
+		header.Add("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept")
+		w.WriteHeader(204)
+		return
+	}
 	h.hander.ServeHTTP(w, req)
 }
