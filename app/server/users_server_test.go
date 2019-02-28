5e348d3fd45806263e1e87c2b351b5f032a13b10
Ryota Egusa <gedorinku@yahoo.co.jp>
2019-02-28 18:38:05 +0900

    Add test

--- app/server/users_server_test.go
+++ app/server/users_server_test.go
@@ -1,1 +1,94 @@
 package server
+
+import (
+	"context"
+	"log"
+	"testing"
+
+	api_pb "github.com/ProgrammingLab/prolab-accounts/api"
+	"github.com/ProgrammingLab/prolab-accounts/app/config"
+	"github.com/ProgrammingLab/prolab-accounts/app/di"
+	"github.com/ProgrammingLab/prolab-accounts/model"
+)
+
+func Test_UsersServer_GetUser(t *testing.T) {
+	cfg, err := config.LoadConfig("../../.env")
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	store := di.MustCreateTestStoreComponent(cfg)
+	defer store.MustClose()
+	ctx := context.Background()
+	users, err := CreateTestUsers(ctx, store)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Get public user", func(t *testing.T) {
+		var req *api_pb.GetUserRequest
+		for _, u := range users {
+			if userProfileScope(model.UserID(u.ID)) != model.Public {
+				continue
+			}
+
+			req = &api_pb.GetUserRequest{
+				UserName: u.Name,
+			}
+			break
+		}
+
+		srv := NewUserServiceServer(store, cfg)
+		resp, err := srv.GetUser(ctx, req)
+
+		if err != nil {
+			t.Errorf("returned an error %v", err)
+		}
+
+		if resp == nil {
+			t.Error("response should not nil")
+		}
+
+		if got, want := resp.Email, ""; got != want {
+			t.Errorf("Email is %v, want %v", got, want)
+		}
+		if got, want := resp.FullName, ""; got != want {
+			t.Errorf("FullName is %v, want %v", got, want)
+		}
+	})
+
+	t.Run("Get members only user", func(t *testing.T) {
+		var req *api_pb.GetUserRequest
+		for _, u := range users {
+			if userProfileScope(model.UserID(u.ID)) != model.MembersOnly {
+				continue
+			}
+
+			req = &api_pb.GetUserRequest{
+				UserName: u.Name,
+			}
+			break
+		}
+
+		srv := NewUserServiceServer(store, cfg)
+		resp, err := srv.GetUser(ctx, req)
+
+		if err != nil {
+			t.Errorf("returned an error %v", err)
+		}
+
+		if resp == nil {
+			t.Error("response should not nil")
+		}
+
+		if got, want := resp.Email, ""; got != want {
+			t.Errorf("Email is %v, want %v", got, want)
+		}
+		if got, want := resp.FullName, ""; got != want {
+			t.Errorf("FullName is %v, want %v", got, want)
+		}
+		if got, want := resp.Description, ""; got != want {
+			t.Errorf("Description is %v, want %v", got, want)
+		}
+	})
+}
