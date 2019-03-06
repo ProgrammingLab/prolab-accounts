95dea46cabe1660dda1ca6998a8219229eb230df
Ryota Egusa <gedorinku@yahoo.co.jp>
2019-03-06 17:39:18 +0900

    Impl CreateInvitation

--- infra/store/invitation/invitation_store.go
+++ infra/store/invitation/invitation_store.go
@@ -2,10 +2,17 @@ package invitationstore
 
 import (
 	"context"
+	"crypto/rand"
 	"database/sql"
+	"encoding/base32"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/volatiletech/sqlboiler/boil"
 
 	"github.com/ProgrammingLab/prolab-accounts/infra/record"
 	"github.com/ProgrammingLab/prolab-accounts/infra/store"
+	"github.com/ProgrammingLab/prolab-accounts/model"
 )
 
 type invitationStoreImpl struct {
@@ -21,6 +28,10 @@ func NewInvitationStore(ctx context.Context, db *sql.DB) store.InvitationStore {
 	}
 }
 
+const (
+	invitationCodeBytes = 32
+)
+
 func (s *invitationStoreImpl) ListInvitations() ([]*record.Invitation, error) {
 	panic("not implemented")
 }
@@ -29,10 +40,36 @@ func (s *invitationStoreImpl) GetInvitation(id int64) (*record.Invitation, error
 	panic("not implemented")
 }
 
-func (s *invitationStoreImpl) CreateInvitation(email string) (*record.Invitation, error) {
-	panic("not implemented")
+func (s *invitationStoreImpl) CreateInvitation(inviter model.UserID, email string) (*record.Invitation, error) {
+	code, err := generateInvitationCode()
+	if err != nil {
+		return nil, err
+	}
+
+	inv := &record.Invitation{
+		Code:      code,
+		Email:     email,
+		InviterID: int64(inviter),
+	}
+	err = inv.Insert(s.ctx, s.db, boil.Infer())
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	return inv, nil
 }
 
 func (s *invitationStoreImpl) DeleteInvitation(id int64) error {
 	panic("not implemented")
+}
+
+func generateInvitationCode() (string, error) {
+	b := make([]byte, invitationCodeBytes)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", errors.WithStack(err)
+	}
+
+	enc := base32.StdEncoding.WithPadding(base32.NoPadding)
+	return strings.ToLower(enc.EncodeToString(b)), nil
 }
--- infra/store/invitation_store.go
+++ infra/store/invitation_store.go
@@ -2,12 +2,13 @@ package store
 
 import (
 	"github.com/ProgrammingLab/prolab-accounts/infra/record"
+	"github.com/ProgrammingLab/prolab-accounts/model"
 )
 
 // InvitationStore provides invitations
 type InvitationStore interface {
 	ListInvitations() ([]*record.Invitation, error)
 	GetInvitation(id int64) (*record.Invitation, error)
-	CreateInvitation(email string) (*record.Invitation, error)
+	CreateInvitation(inviter model.UserID, email string) (*record.Invitation, error)
 	DeleteInvitation(id int64) error
 }
