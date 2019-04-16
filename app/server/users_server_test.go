a692940acdd9df8135bca03c25bc812ab4cc05cb
Ryota Egusa <egusa.ryota@gmail.com>
2019-04-16 11:57:55 +0900

    Update test

--- app/server/users_server_test.go
+++ app/server/users_server_test.go
@@ -8,6 +8,7 @@ import (
 	api_pb "github.com/ProgrammingLab/prolab-accounts/api"
 	"github.com/ProgrammingLab/prolab-accounts/app/config"
 	"github.com/ProgrammingLab/prolab-accounts/app/di"
+	"github.com/ProgrammingLab/prolab-accounts/app/util"
 	"github.com/ProgrammingLab/prolab-accounts/model"
 )
 
@@ -73,22 +74,12 @@ func Test_UsersServer_GetUser(t *testing.T) {
 		srv := NewUserServiceServer(store, cfg)
 		resp, err := srv.GetUser(ctx, req)
 
-		if err != nil {
-			t.Errorf("returned an error %v", err)
+		if got, want := err, util.ErrNotFound; got != want {
+			t.Errorf("error shold be %v, but %v", want, got)
 		}
 
-		if resp == nil {
-			t.Error("response should not nil")
-		}
-
-		if got, want := resp.Email, ""; got != want {
-			t.Errorf("Email is %v, want %v", got, want)
-		}
-		if got, want := resp.FullName, ""; got != want {
-			t.Errorf("FullName is %v, want %v", got, want)
-		}
-		if got, want := resp.Description, ""; got != want {
-			t.Errorf("Description is %v, want %v", got, want)
+		if got := resp; got != nil {
+			t.Errorf("resp shold be %v, but %v", nil, got)
 		}
 	})
 }
